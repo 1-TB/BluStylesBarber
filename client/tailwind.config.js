4fdaf4f66c334d6a2607b9e6daee75d82b3ebe05
ThatzMeJ <jkmcgarrah@gmail.com>
2024-10-24 23:56:56 -0500

    Added Introduction section and other minor updates.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        teko: ['"Teko"', 'sans-serif'],
+        abel: ['"Abel"', 'sans-serif'] 
+      }
+    },
   },
   plugins: [],
-}
+};
